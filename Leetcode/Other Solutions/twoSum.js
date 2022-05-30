dbadce42069a09dcd84382cd143efa5fd36aad8a
CJM1994 <connorjohnmullin@gmail.com>
2022-05-30 13:13:11 -0700

    nots

--- Leetcode/Other Solutions/twoSum.js
+++ Leetcode/Other Solutions/twoSum.js
@@ -18,3 +18,7 @@ var twoSum = function (nums, target) {
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// This is a less good solution than creating a table with the number we need to create a pair
+// and then looping through once to find that
+// In an interview use the more optimal solution
--- Leetcode/Other Solutions/twoSum.ts
+++ Leetcode/Other Solutions/twoSum.ts
@@ -13,3 +13,5 @@ var twoSum = function (nums: number[], target: number): number[] {
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// This is a non-optimal solution to this problem, just converting to typescript
