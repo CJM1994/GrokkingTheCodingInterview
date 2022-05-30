f0bdf54b15396170d58210bc86a07378598c98ca
CJM1994 <connorjohnmullin@gmail.com>
2022-05-30 13:06:47 -0700

    more solutions

---
+++ Leetcode/Other Solutions/twoSum.ts
@@ -0,0 +1,15 @@
+var twoSum = function (nums: number[], target: number): number[] {
+  let startIndex = 1;
+  for (const [numIndex, numValue] of nums.entries()) {
+    for (let i = startIndex; i <= nums.length; i++) {
+      if (numValue + nums[i] === target) {
+        return [numIndex, i];
+      }
+    }
+    startIndex++;
+  }
+};
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
